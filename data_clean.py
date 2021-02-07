7f72a33f718e64616c5a7195d4fd346789bb8407
Olesia Khrapunova <64822688+olesiaskh@users.noreply.github.com>
2021-02-07 22:12:46 +0100

    Update data_clean.py

--- data_clean.py
+++ data_clean.py
@@ -1,16 +1,10 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 21 13:07:10 2020
-
-@author: olesiakhrapunova
-"""
-
-
 def tld_clean(data):
-    #update org and tld elements
-    ex_org=[]
-    exceptions = ['org','co','gov','ac','net']
+    """
+    Separate organization name and domain name if they are still together in the 'tld' column
+    Replace the current organization name with the extracted organization name
+    Double domain names (such as 'gov.fr' are kept as one domain name)
+    """
+    exceptions = ['org','co','gov','ac','net'] #elements of double domain names
     for index,row in data.iterrows():
         element = row['tld']
         if type(element) != float:
@@ -18,46 +12,32 @@ def tld_clean(data):
             if len(splitted) == 2:
                 if splitted[0] not in exceptions:
                     data.loc[index,'tld'] = splitted[1]
-                    first_org=row['org']
-                    ex_org.append(first_org)
                     data.loc[index,'org']=splitted[0]
-                else:
-                    ex_org.append(None)
       
             elif len(splitted) > 2:
                 if splitted[-2] not in exceptions:
                     data.loc[index,'tld'] = splitted[-1]
-                    first_org=row['org']
-                    ex_org.append(first_org)
                     merged_org = '.'.join(splitted[:-2])
                     data.loc[index,'org'] = merged_org
                 else:
                      merged_tld = '.'.join(splitted[-2:-1])
                      data.loc[index,'tld'] = merged_tld
-                     first_org=row['org']
-                     ex_org.append(first_org)
                      merged_org = '.'.join(splitted[:-3])
                      data.loc[index,'org'] = merged_org
-            else:
-                ex_org.append(None)
+
         else:
             data.loc[index,'tld']=None
-            ex_org.append(None)
-         
-    #insert the new column
-    data.insert(2,"ex_org",ex_org)
-    
+           
     return data
 
 
-def mailtype_clean(df):
-    df['mail_type'] = df['mail_type'].replace(['Multipart/Alternative'],'multipart/alternative')
-    df['mail_type'] = df['mail_type'].replace(['Multipart/Mixed'],'multipart/mixed')
-    df['mail_type'] = df['mail_type'].replace(['Text/Html'],'text/html')
-    df['mail_type'] = df['mail_type'].replace(['text/html '],'text/html')
+def mailtype_clean(data):
+    """
+    Fix differences in spelling of the same values
+    """
+    data['mail_type'] = data['mail_type'].replace(['Multipart/Alternative'],'multipart/alternative')
+    data['mail_type'] = data['mail_type'].replace(['Multipart/Mixed'],'multipart/mixed')
+    data['mail_type'] = data['mail_type'].replace(['Text/Html'],'text/html')
+    data['mail_type'] = data['mail_type'].replace(['text/html '],'text/html')
     
-    return df
-
-
-
-
+    return data
