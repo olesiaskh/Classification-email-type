9b999ba64e849d2954c804b4276b467ebd7ab622
Olesia Khrapunova <64822688+olesiaskh@users.noreply.github.com>
2021-02-07 22:01:13 +0100

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -1,8 +1,6 @@
 from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 
-
-
 def encode_train_test(test, train):
     feat_enc = OneHotEncoder()
     both = pd.concat([test, train])
@@ -10,10 +8,4 @@ def encode_train_test(test, train):
     test_x_featurized = pd.DataFrame(feat_enc.transform(test).toarray())
     train_x_featurized = pd.DataFrame(feat_enc.transform(train).toarray())
     return test_x_featurized, train_x_featurized
-
-def encode_train(train):
-    feat_enc = OneHotEncoder(handle_unknown = 'ignore' )
-    feat_enc.fit(train)
-    train_x_featurized = pd.DataFrame(feat_enc.transform(train).toarray())
-    return train_x_featurized
     
