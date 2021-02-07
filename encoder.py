831d2f372ab111347db303b367ba91e9dfb4e850
Olesia Khrapunova <64822688+olesiaskh@users.noreply.github.com>
2021-02-07 22:14:16 +0100

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -2,6 +2,10 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 
 def encode_train_test(test, train):
+    """
+    One-hot encode categorical features for both train and test datasets.
+    The encoding step takes into account values present in both datasets.
+    """
     feat_enc = OneHotEncoder()
     both = pd.concat([test, train])
     feat_enc.fit(both)
