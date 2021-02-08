11d5f068c616bf61f300f7fe207a6e2565ad7e98
Olesia Khrapunova <64822688+olesiaskh@users.noreply.github.com>
2021-02-08 21:05:27 +0100

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -1,15 +1,30 @@
 from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 
-def encode_train_test(test, train):
-    """
-    One-hot encode categorical features for both train and test datasets.
-    The encoding step takes into account values present in both datasets.
+def encode_train_test(train, test):
+    """One-hot encodes train and test data
+    
+    Parameters
+    ----------
+    train: dataframe
+        Dataframe with features from train data to be encoded.
+    test: dataframe
+        Dataframe with features from test data to be encoded.
+        
+    Returns
+    -------
+    train_x_featurized: dataframe
+        Dataframe with encoded features from train data.
+    test_x_featurized: dataframe
+        Dataframe with encoded features from test data.
+        
+    Notes
+    -----
+    Features included in the two datasets should be identical.    
     """
     feat_enc = OneHotEncoder()
-    both = pd.concat([test, train])
+    both = pd.concat([train, test])
     feat_enc.fit(both)
-    test_x_featurized = pd.DataFrame(feat_enc.transform(test).toarray())
     train_x_featurized = pd.DataFrame(feat_enc.transform(train).toarray())
-    return test_x_featurized, train_x_featurized
-    
+    test_x_featurized = pd.DataFrame(feat_enc.transform(test).toarray())
+    return train_x_featurized, test_x_featurized
