0a7fc09d73df05208ff1aa38247c38563b90e181
Olesia Khrapunova <khrapunova@uchicago.edu>
2024-01-02 17:02:34 -0600

    simpified functions

--- data_clean.py
+++ data_clean.py
@@ -1,64 +1,61 @@
+import pandas as pd
+
+
 def tld_clean(data):
     """Separate organization and domain if they are still one value in 'tld'.
-    
+
     Parameters
     ----------
     data: dataframe
         Dataframe that includes 'tld' and 'org' columns.
-        
+
     Returns
     -------
     dataframe
         Dataframe with cleaned values in 'tld'.
-        
+
     Notes
     -------
     The function replaces current organization name with name extracted from 'tld'.
     Double domain names (such as 'gov.fr') are kept as one value in 'tld'.
     """
-    exceptions = ['org','co','gov','ac','net'] #elements of double domain names
-    for index,row in data.iterrows():
-        element = row['tld']
-        if type(element) != float:
+    exceptions = ["org", "co", "gov", "ac", "net"]  # elements of double domain names
+    for index, row in data.iterrows():
+        element = row["tld"]
+        if pd.notna(element):
             splitted = element.split(".")
-            if len(splitted) == 2:
-                if splitted[0] not in exceptions:
-                    data.loc[index,'tld'] = splitted[1]
-                    data.loc[index,'org']=splitted[0]
-      
+            if len(splitted) == 2 and splitted[0] not in exceptions:
+                data.loc[index, "tld"] = splitted[1]
+                data.loc[index, "org"] = splitted[0]
+
             elif len(splitted) > 2:
                 if splitted[-2] not in exceptions:
-                    data.loc[index,'tld'] = splitted[-1]
-                    merged_org = '.'.join(splitted[:-2])
-                    data.loc[index,'org'] = merged_org
+                    data.loc[index, "tld"] = splitted[-1]
+                    data.loc[index, "org"] = ".".join(splitted[:-2])
                 else:
-                     merged_tld = '.'.join(splitted[-2:-1])
-                     data.loc[index,'tld'] = merged_tld
-                     merged_org = '.'.join(splitted[:-3])
-                     data.loc[index,'org'] = merged_org
+                    data.loc[index, "tld"] = ".".join(splitted[-2:-1])
+                    data.loc[index, "org"] = ".".join(splitted[:-3])
 
         else:
-            data.loc[index,'tld']=None
-           
+            data.loc[index, "tld"] = None
+
     return data
 
 
 def mailtype_clean(data):
     """Fix differences in spelling of the same values.
-    
+
     Parameters
     ----------
     data: dataframe
         Dataframe that includes the 'mail_type' column with values to fix.
-        
+
     Returns
     -------
     dataframe
-        Dataframe with spelling fixed in 'mail_type' column.
+        Dataframe with values in 'mail_type' column lowered and
+        stripped of trailing whitespaces.
     """
-    data['mail_type'] = data['mail_type'].replace(['Multipart/Alternative'],'multipart/alternative')
-    data['mail_type'] = data['mail_type'].replace(['Multipart/Mixed'],'multipart/mixed')
-    data['mail_type'] = data['mail_type'].replace(['Text/Html'],'text/html')
-    data['mail_type'] = data['mail_type'].replace(['text/html '],'text/html')
-    
+    data["mail_type"] = data["mail_type"].str.lower().str.strip()
+
     return data
--- new_features.py
+++ new_features.py
@@ -1,114 +1,55 @@
 def hour_day_month_utc(data):
     """Create hour, day, month and UTC features based on date.
-    
+
     Parameters
     ----------
     data: dataframe
         Dataframe that includes 'date' column.
-        
+
     Returns
     -------
     dataframe
         Dataframe with 4 new columns: 'Hour', 'Day', 'Month', 'UTC'.
     """
-    hours=[]
-    day=[]
-    month=[]
-    utc=[]
-    dates = data[['date']].date
-    for element in dates:
-        splited = element.split(" ")
-        if len(splited)>=6:
-            hrs = splited[4].split(":")
-            hr= hrs[0]
-            u = splited[5]
-            utc.append(u)
-            if len(hr)==2:
-              hours.append(hr)
-            else: 
-              hours.append(None)
-        elif len(splited)>=4:
-            utc.append(None)
-            hrs = splited[3].split(":")
-            hr= hrs[0]
-            if len(hr)==2:
-              hours.append(hr)
-            else: 
-              hours.append(None)
-        else:
-            hours.append(None)
-            utc.append(None)
-  
-        d = splited[0]
-        if len(d)==4:
-            day.append(d[:-1])
-        else:
-            day.append(None)
-        
-        if len(splited)>=3:
-            if len(splited[2])==3:
-              month.append(splited[2])
-            else: 
-              month.append(None)
-        else:
-            month.append(None)
+    data["Hour"] = data["date"].str.extract(r"(\d{2}):\d{2}:\d{2}")
+    data["Day"] = data["date"].str.extract(r"\d{4}-\d{2}-(\d{2})")
+    data["Month"] = data["date"].str.extract(r"\d{4}-(\d{2})-\d{2}")
+    data["UTC"] = (
+        data["date"]
+        .str.extract(r"\b(GMT| -0000)\b")
+        .replace(["GMT", " -0000"], "+0000")
+    )
 
-        data.insert(2,"Hour",hours)
-        data.insert(3,"Day",day)
-        data.insert(4,"Month",month)
-        data.insert(5,"UTC",utc)
-        
-        
-        data['UTC'] = data['UTC'].replace(['GMT'],'+0000')
-        data['UTC'] = data['UTC'].replace(['-0000'],'+0000')
-        
     return data
 
 
-def weekend_month(data):
-    """Create weekend, month features based on date.
-    
+def weekend_flag(data):
+    """Create weekend flag based on date.
+
     Parameters
     ----------
     data: dataframe
         Dataframe that includes 'date' column.
-        
+
     Returns
     -------
     dataframe
-        Dataframe with 2 new columns: 'weekend', 'month'.
+        Dataframe with new boolean column: 'weekend'.
     """
-    weekend = ['Sat,','Sun,']
-    month = []
-    for index,row in data.iterrows():
-        element = data.loc[index,'date']
-        splited = element.split(" ")
-        if splited[0] in weekend:
-            data.loc[index,'weekend'] = 1
-        else:
-            data.loc[index,'weekend'] = 0
-        if len(splited)>=3:
-            if len(splited[2])==3:
-                month.append(splited[2])
-            else: 
-                month.append('None')
-        else:
-          month.append('None')
-      
-    data.insert(2,"month", month)
+    weekend_days = ["Sat,", "Sun,"]
+    data["weekend"] = data["date"].str.split().str[0].isin(weekend_days).astype(int)
 
     return data
 
 
-
 def orgtype(data):
     """Define sets of organization types.
-    
+
     Parameters
     ----------
     data: dataframe
         Dataframe that includes 'label' and 'org' columns.
-        
+
     Returns
     -------
     org_update: set
@@ -124,44 +65,57 @@ def orgtype(data):
     org_travel: set
         The organizations the belong to set 'travel'.
     org_spam: set
-        The organizations the belong to set 'spam'. 
+        The organizations the belong to set 'spam'.
     org_social: set
         The organizations the belong to set 'social'.
-        
+
     Notes
     -------
     The type is based on target value that appears with a specific organization.
     If an organization is associated with multiple target values, the organization is
-    assigned to the type that is least common in the dataset overall 
+    assigned to the type that is least common in the dataset overall
     (neither 'update', nor 'personal').
     """
-    label_0 = set(data[data['label']==0]['org'].unique())
-    label_1 = set(data[data['label']==1]['org'].unique())
-    label_2 = set(data[data['label']==2]['org'].unique())
-    label_3 = set(data[data['label']==3]['org'].unique())
-    label_4 = set(data[data['label']==4]['org'].unique())
-    label_5 = set(data[data['label']==5]['org'].unique())
-    label_6 = set(data[data['label']==6]['org'].unique())
-    label_7 = set(data[data['label']==7]['org'].unique())
-    
-    org_update = label_0 - set.intersection(label_0,label_2) -  set.intersection(label_0,label_3) -  set.intersection(label_0,label_4)- set.intersection(label_0,label_5)- set.intersection(label_0,label_6)- set.intersection(label_0,label_7)
-    org_personal = label_1 - set.intersection(label_0, label_1) - set.intersection(label_1,label_2) -  set.intersection(label_1,label_3) -  set.intersection(label_1,label_4)- set.intersection(label_1,label_5)- set.intersection(label_1,label_6)- set.intersection(label_1,label_7)
-    org_promo = label_2 - set.intersection(label_2,label_4)
-    org_prof = label_3 
-    org_com = label_4 
-    org_travel = label_5 - set.intersection(label_5, label_3)
-    org_spam = label_6 - set.intersection(label_6, label_4)
-    org_social = label_7 
+    label_sets = [set(data[data["label"] == i]["org"].unique()) for i in range(8)]
 
-    return org_update, org_personal, org_promo, org_prof, org_com, org_travel, org_spam, org_social
-    
-def org_encode(data, org_update, org_personal, org_promo, org_prof, org_com, org_travel, org_spam, org_social):
+    org_update = label_sets[0] - set.union(*label_sets[2:])
+    org_personal = label_sets[1] - set.union(label_sets[0], *label_sets[2:])
+    org_promo = label_sets[2] - label_sets[4]
+    org_prof = label_sets[3]
+    org_com = label_sets[4]
+    org_travel = label_sets[5] - label_sets[3]
+    org_spam = label_sets[6] - label_sets[4]
+    org_social = label_sets[7]
+
+    return (
+        org_update,
+        org_personal,
+        org_promo,
+        org_prof,
+        org_com,
+        org_travel,
+        org_spam,
+        org_social,
+    )
+
+
+def org_encode(
+    data,
+    org_update,
+    org_personal,
+    org_promo,
+    org_prof,
+    org_com,
+    org_travel,
+    org_spam,
+    org_social,
+):
     """Assign organization type to each email.
-    
+
     Parameters
     ----------
     data: dataframe
-        Dataframe that includes 'label' and 'org' columns.
+        Dataframe that includes 'org' column.
     org_update: set
         The organizations the belong to set 'update'.
     org_personal: set
@@ -175,67 +129,27 @@ def org_encode(data, org_update, org_personal, org_promo, org_prof, org_com, org
     org_travel: set
         The organizations the belong to set 'travel'.
     org_spam: set
-        The organizations the belong to set 'spam'. 
+        The organizations the belong to set 'spam'.
     org_social: set
-        The organizations the belong to set 'social'.  
-        
+        The organizations the belong to set 'social'.
+
     Returns
     -------
     dataframe
-        Dataframe with 8 new columns: 'org_update', 'org_personal', 
-        'org_promo', 'org_prof', 'org_com', 'org_travel', 'org_spam', 
+        Dataframe with 8 new columns: 'org_update', 'org_personal',
+        'org_promo', 'org_prof', 'org_com', 'org_travel', 'org_spam',
         'org_social' (all boolean).
     """
-    for index, row in data.iterrows():
-        if data.loc[index,'org'] == data.loc[index,'org']:
-            if data.loc[index,'org'] in org_update:
-                data.loc[index,'org_update']=1
-            else:
-                data.loc[index,'org_update']=0
-                
-            if data.loc[index,'org'] in org_personal:
-                data.loc[index,'org_personal']=1
-            else:
-                data.loc[index,'org_personal']=0
-                
-            if data.loc[index,'org'] in org_promo:
-                data.loc[index,'org_promo']=1
-            else:
-                data.loc[index,'org_promo']=0
-                
-            if data.loc[index,'org'] in org_prof:
-                data.loc[index,'org_prof']=1
-            else:
-                data.loc[index,'org_prof']=0    
-                
-            if data.loc[index,'org'] in org_com:
-                data.loc[index,'org_com']=1
-            else:
-                data.loc[index,'org_com']=0   
-                
-            if data.loc[index,'org'] in org_travel:
-                data.loc[index,'org_travel']=1
-            else:
-                data.loc[index,'org_travel']=0
-                
-            if data.loc[index,'org'] in org_spam:
-                data.loc[index,'org_spam']=1
-            else:
-                data.loc[index,'org_spam']=0
-                
-            if data.loc[index,'org'] in org_social:
-                data.loc[index,'org_social']=1
-            else:
-                data.loc[index,'org_social']=0
-                
-        else:
-            data.loc[index,'org_update']=0
-            data.loc[index,'org_personal']=0
-            data.loc[index,'org_promo']=0 
-            data.loc[index,'org_prof']=0  
-            data.loc[index,'org_com']=0 
-            data.loc[index,'org_travel']=0 
-            data.loc[index,'org_spam']=0
-            data.loc[index,'org_social']=0
-            
-    return data        
+    for org_type in [
+        "org_update",
+        "org_personal",
+        "org_promo",
+        "org_prof",
+        "org_com",
+        "org_travel",
+        "org_spam",
+        "org_social",
+    ]:
+        data[org_type] = data["org"].isin(eval(org_type)).astype(int)
+
+    return data
